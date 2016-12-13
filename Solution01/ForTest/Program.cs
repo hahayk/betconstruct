e9356df834f1591a861bae2401e5d56035e591c8
Hayk Haru <hayk.haru@gmail..com>
2016-12-13 16:00:51 +0400

    Changes in Lesson 3

---
+++ Solution01/ForTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ForTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(killKthBit(37, 3));
+        }
+
+        static int killKthBit(int n, int k)
+        {
+            string s1 = Convert.ToString(37, 2);
+            int len = s1.Length;
+
+            var array = Enumerable
+              .Repeat('1', s1.Length)
+              .ToArray();
+            array[k] = '0';
+
+            Console.WriteLine(Convert.ToInt32(s1, 10));
+
+            return 0;
+        }
+    }
+}
