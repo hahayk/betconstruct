09606738104697f0e6980355aeb524bd147f8710
Hayk Harutyunyan <hayk.haru@gmail..com>
2017-01-19 21:14:13 +0400

    Generik Example

---
+++ Solution07/ConsoleApplication/GenericExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class GenericExample<T> where T: Exception
+    {
+        public T F(T value)
+        {
+            return value;
+        }
+    }
+
+    public class AnotherGenericExample<U, V, T> : GenericExample<T> where T : SystemException
+    {
+
+    }
+}
---
+++ Solution07/ConsoleApplication/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object x = new object();
+            object y = new object();
+            object temp = new object();
+
+            temp = x;
+            x = y;
+            y = temp;
+
+            Swap<object>(x, y);
+            Swap(x, y);
+
+            object[] result1 = Gen<int, double, object>();
+            object[] result2 = Gen<int, double, object>(1, 2.0);
+
+            var gen1 = new GenericExample<InvalidCastException>();
+            var gen2 = new AnotherGenericExample<int, double, OutOfMemoryException>();
+        }
+        static void Swap<T>(T x, T y) //where T: class, IEnumerable, new()
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+
+        static T[] Gen<T1, T2, T>()
+            where T1 : T
+            where T2 : T
+        {
+            return new T[] { };
+        }
+
+
+        static T[] Gen<T1, T2, T>(T1 a, T2 b)
+            where T1 : T
+            where T2 : T
+        {
+            return new T[] { a, b };
+        }
+    }
+}
