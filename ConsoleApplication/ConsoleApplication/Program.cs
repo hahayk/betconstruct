b9ae8a5c6e06441101d7820580015b0f7e5eee19
Hayk Harutyunyan <hayk.haru@gmail..com>
2017-01-21 22:19:29 +0400

    Delegates

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        public delegate void MyAction();
+        public delegate double Function(double a);
+
+        static void Main(string[] args)
+        {
+            MyAction f = DoWork;
+
+            var obj = new MyType();
+            MyAction f1 = obj.DoOtherWork;
+
+            DoWork();
+            f();
+
+            obj.DoOtherWork();
+            f1();
+
+            var s1 = Integrate(Math.Sin, 0, Math.PI);
+            var s2 = Integrate(Math.Cos, 0, Math.PI);
+
+
+        }
+
+        static void DoWork()
+        {
+            Console.WriteLine("I did it!");
+        }
+
+        static double Integrate(Function f, double a, double b)
+        {
+            //Integration algorithm
+
+            //Exampl
+            return (f(a) + f(b)) / 2D; //(2D = 2.0)
+        }
+    }
+
+    class MyType
+    {
+        public void DoOtherWork()
+        {
+            Console.WriteLine("I did it again!");
+        }
+    }
+}
---
+++ ConsoleApplication/DelegateConsoleApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateConsoleApp
+{
+    class Program
+    {
+        delegate double Function(double x);
+        
+
+        static void Main(string[] args)
+        {
+
+            var ls = new LinkedListNode<string>("");
+            
+
+            Function sin = Math.Sin;
+
+            var x = new double[100];
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] = 0.01 * i;
+            }
+
+            //var y1 = CalculateValues(x, sin);
+            var y1 = CalculateValues(x, Math.Sin);
+            
+            //with lambda expression;
+            Function f1 = Falt(2.0, 5.0, -1);
+            Function f2 = FL(2.0, 5.0, -1);
+
+            var y2 = CalculateValues(x, f1);
+            var y3 = CalculateValues(x, f2);
+
+            //anonymouse function
+            Function f = delegate (double z)
+            {
+                return z;
+            };
+
+        }
+
+        static double[] CalculateValues(double[] arguments, Function f)
+        {
+            var values = new double[arguments.Length];
+
+            for (int i = 0; i < arguments.Length; i++)
+            {
+                values[i] = f(arguments[i]);
+            }
+
+            return values;
+        }
+
+        static double F(double x)
+        {
+            return 2.0 * x * x + 7.0 * x + 2;
+        }
+
+        static Function FL(double a, double b, double c)
+        {
+            return x => a * x * x + b * x + c;
+        }
+
+        static Function Falt(double a, double b, double c)
+        {
+            Function f = delegate (double x) { return a * x * x + b * x + c; };
+
+            return f;
+        }
+
+
+    }
+}
