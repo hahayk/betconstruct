a92deeff2d92c88002bf50b0021c03ab0f0a1d39
Hayk Harutyunyan <hayk.haru@gmail..com>
2017-01-24 14:36:28 +0400

    Linq Example

---
+++ LINQ_Examples/LINQ_Examples/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strings = new string[] { "Hello", "world", "Hi", "Hello", "World", "World" };
+
+            var group1 = strings.GroupBy(s => s);
+            var group2 = strings.GroupBy(s => s.Length);
+
+            PrintGroup(group1);
+            PrintGroup(group2);
+
+
+            var numbers = Enumerable.Range(0, 100);
+            var evenOdd = numbers.GroupBy(x => x % 2 == 0 ? "Even": "Odd");
+            PrintGroup(evenOdd); 
+        }
+
+        static void PrintGroup<U, V>(IEnumerable<IGrouping<U, V>> group)
+        {
+            foreach (var item in group)
+            {
+                Console.WriteLine($"{item.Key}");
+
+                foreach (var value in item)
+                {
+                    Console.WriteLine($"\t{value}");
+                }
+            }
+        }
+    }
+
+}
