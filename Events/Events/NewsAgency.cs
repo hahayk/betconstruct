4af7cc389a524c4bdd8e5d65678c76044acd51b8
Hayk Harutyunyan <hayk.haru@gmail..com>
2017-01-24 15:35:44 +0400

    Events

---
+++ Events/Events/NewsAgency.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class NewsAgency
+    {
+        public string Name { get; set; }
+        public NewsAgency(string name)
+        {
+            Name = name;
+        }
+
+
+        public event EventHandler BreakingNews;
+
+        public void BroadcastBreakingNews ()
+        {
+            //checking news
+            BreakingNews?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Events/Events/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newsAgencies = new NewsAgency[] {
+                new NewsAgency("BBC"),
+                new NewsAgency("CNN"),
+                new NewsAgency("ABC")
+            };
+
+
+            foreach (var item in newsAgencies)
+            {
+                item.BreakingNews += ShowBreakingNews;
+            }
+
+            foreach (var item in newsAgencies)
+            {
+                item.BroadcastBreakingNews();
+            }
+
+
+            //var a = new NewsAgency("BBC");
+            //a.BreakingNews += BBC_BreakingNews;
+            //a.BreakingNews += (sender, e) => { };
+
+            //var b = new NewsAgency("CNN");
+            //b.BreakingNews += BBC_BreakingNews;
+
+            //a.BroadcastBreakingNews();
+            //b.BroadcastBreakingNews();
+
+        }
+
+        private static void ShowBreakingNews(object sender, EventArgs e)
+        {
+            var agency = sender as NewsAgency;
+
+            if (agency != null)
+            {
+                Console.WriteLine($"{agency.Name}: {e.ToString()}");
+            }
+        }
+    }
+}
