305230ea3d6d89622d7e3971582ff197658c3903
Hayk Harutyunyan <hayk.haru@gmail.com>
2017-02-05 00:18:56 +0400

    update

--- Solution11/ConsoleApplication/Program.cs
+++ Solution11/ConsoleApplication/Program.cs
@@ -75,7 +75,5 @@ namespace ConsoleApplication
             }
 
         }
-
-
     }
 }
---
+++ Solution11/ConsoleApplicationTaskFactory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationTaskFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Run(() => {
+
+            });
+
+        }
+    }
+}
