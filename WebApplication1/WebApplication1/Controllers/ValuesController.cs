09abb34b3f16f5fb7a26c1470d2c825861f0fcfc
Hayk Harutyunyan <hayk.haru@gmail.com>
2017-04-14 14:04:58 +0400

    WEBApi

---
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Controllers.Models;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("api")]
+    public class ValuesController : ApiController
+    {
+        [Route("values")]
+        [HttpGet]
+        public  IHttpActionResult GetValues()
+        {
+            var resutl = new string[]
+            {
+                "Hello World",
+                "Բարեւ Արցախ"
+            };
+
+            return Ok(resutl);
+        }
+
+
+        [Route("values")]
+        [HttpPost]
+        public IHttpActionResult SendValues(Person model)
+        {
+            if (model == null)
+            {
+                return BadRequest("model is null");
+            }
+            model.Age++;
+            model.Name += "!";
+
+            if (model.Age < 18)
+            {
+                return BadRequest();
+            }
+
+            return Ok(model);
+        }
+
+    }
+}
